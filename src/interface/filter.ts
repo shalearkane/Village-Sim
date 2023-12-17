b292dd961f91f6a78b540153c8a531170f30e269
Shubhrajyoti-Dey-FrosTiK <apple@Apples-MacBook-Pro.local>
2023-12-17 18:47:12 +0530

    Added support for your position in minimap

--- src/interface/filter.ts
+++ src/interface/filter.ts
@@ -7,4 +7,8 @@ export interface FilterOptions {
   [GeoDataType.SEWAGE_TREATMENT]: boolean;
   [GeoDataType.COMMERCIAL]: boolean;
   [GeoDataType.ROAD]: boolean;
+  [GeoDataType.ELECTRICITY]: boolean;
+  [GeoDataType.ADMINISTRATION]: boolean;
+  [GeoDataType.WATER_SUPPLY]: boolean;
+  YOUR_POSITION: boolean;
 }
