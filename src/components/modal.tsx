38ca784d0c6df99281637f72f789f5f15ac92b66
Sakshi Sharan <sakshisharan9501@gmail.com>
2023-12-19 15:16:41 +0530

    Updating info Modal with metadata

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -23,53 +23,67 @@ function InfoModal() {
             {mouseControl.clickInfo?.geoDataPoint?.type}
           </p>
           <p className="">
-            <b>Nearest Hospital: </b>
-            {mouseControl.clickInfo?.geoDataPoint?.metadata?.hospitalDistance}
-          </p>
-          <p className="">
             <b>Nearest road: </b>
-            {mouseControl.clickInfo?.geoDataPoint?.metadata?.roadDistance}
+            {mouseControl.clickInfo?.geoDataPoint?.metadata?.roadDistance?.dist}
           </p>
           <p className="">
             <b>Nearest Residential: </b>
             {
               mouseControl.clickInfo?.geoDataPoint?.metadata
-                ?.residentialDistance
+                ?.residentialDistance?.dist
             }
           </p>
           <p className="">
             <b>Nearest Hospital: </b>
-            {mouseControl.clickInfo?.geoDataPoint?.metadata?.hospitalDistance}
+            {mouseControl.clickInfo?.geoDataPoint?.metadata?.healthcare?.dist}
           </p>
           <p className="">
             <b>Nearest Agriculture field: </b>
             {
               mouseControl.clickInfo?.geoDataPoint?.metadata
-                ?.agriculturalDistance
+                ?.agriculturalDistance?.dist
+            }
+          </p>
+          <p className="">
+            <b>Nearest Administration: </b>
+            {
+              mouseControl.clickInfo?.geoDataPoint?.metadata
+                ?.administrative?.dist
             }
           </p>
           <p className="">
             <b>Nearest Shop: </b>
-            {mouseControl.clickInfo?.geoDataPoint?.metadata?.commercialDistance}
+            {mouseControl.clickInfo?.geoDataPoint?.metadata?.commercialDistance?.dist}
           </p>
           <p className="">
             <b>Nearest Factory: </b>
-            {mouseControl.clickInfo?.geoDataPoint?.metadata?.industrialDistance}
+            {mouseControl.clickInfo?.geoDataPoint?.metadata?.industrialDistance?.dist}
+          </p>
+          <p className="">
+            <b>Nearest Electric Facility: </b>
+            {mouseControl.clickInfo?.geoDataPoint?.metadata?.electric_facility?.dist}
           </p>
           <p className="">
             <b>Nearest School: </b>
-            {mouseControl.clickInfo?.geoDataPoint?.metadata?.schoolDistance}
+            {mouseControl.clickInfo?.geoDataPoint?.metadata?.school?.dist}
           </p>
           <p className="">
             <b>Nearest Sewage Treatment Plant: </b>
             {
               mouseControl.clickInfo?.geoDataPoint?.metadata
-                ?.sewageTreatmentDistance
+                ?.sanitation?.dist
+            }
+          </p>
+          <p className="">
+            <b>Nearest Water Supply Plant: </b>
+            {
+              mouseControl.clickInfo?.geoDataPoint?.metadata
+                ?.water_facility?.dist
             }
           </p>
           <p className="">
             <b>Nearest Water Body: </b>
-            {mouseControl.clickInfo?.geoDataPoint?.metadata?.waterBodyDistance}
+            {mouseControl.clickInfo?.geoDataPoint?.metadata?.waterBodyDistance?.dist}
           </p>
 
           <div className="text-center mt-5">
